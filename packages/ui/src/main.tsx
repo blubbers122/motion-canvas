cbcf0a68090ce180d456cd66d6785b59ceabc62a
Jacob <64662184+aarthificial@users.noreply.github.com>
2024-08-28 03:05:44 +0200

    feat: keyboard navigation for scene graph (#1101)

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -12,14 +12,11 @@ import {
 import {ComponentChild, render} from 'preact';
 import {Editor} from './Editor';
 import {ProjectData, ProjectSelection} from './ProjectSelection';
-import {ApplicationProvider, PanelsProvider} from './contexts';
 import {
-  GLOBAL_EDITOR_SHORTCUTS,
-  GLOBAL_PRESENTER_SHORTCUTS,
+  ApplicationProvider,
+  PanelsProvider,
   ShortcutsProvider,
-  TIMELINE_SHORTCUTS,
-  VIEWPORT_SHORTCUTS,
-} from './contexts/shortcuts';
+} from './contexts';
 import GridPlugin from './plugin/GridPlugin';
 import {projectNameSignal} from './signals';
 import {getItem, setItem} from './utils';
@@ -29,6 +26,8 @@ const ExperimentalHooks = [
   'provider',
   'previewOverlay',
   'presenterOverlay',
+  'inspectors',
+  'shortcuts',
 ] as const;
 
 function renderRoot(vnode: ComponentChild) {
@@ -169,14 +168,7 @@ export function editor(project: Project) {
       }}
     >
       <PanelsProvider>
-        <ShortcutsProvider
-          configs={[
-            GLOBAL_EDITOR_SHORTCUTS,
-            GLOBAL_PRESENTER_SHORTCUTS,
-            TIMELINE_SHORTCUTS,
-            VIEWPORT_SHORTCUTS,
-          ]}
-        >
+        <ShortcutsProvider>
           <Editor />
         </ShortcutsProvider>
       </PanelsProvider>
